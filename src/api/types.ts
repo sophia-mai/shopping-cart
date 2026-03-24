cbefa35839aaeacad17b316549af2666be0bdee8
unknown <sophiathanhmai@gmail.com>
2026-03-24 09:05:24 -0400

    shopping-cart-02: Data types and API functions

---
+++ src/api/types.ts
@@ -0,0 +1,10 @@
+export type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  category: string;
+  rating: number;
+  thumbnail: string;
+  images: string[];
+};
